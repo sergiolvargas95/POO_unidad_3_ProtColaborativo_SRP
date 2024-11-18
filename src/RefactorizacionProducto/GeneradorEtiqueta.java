f248c21f6a45ac90576fe738cf0da729372e3ec5
sr.davidsantiago@hotmail.com <sr.davidsantiago@hotmail.com>
2024-11-18 15:29:12 -0500

    Terminar ejercicio de Refactorización de Producto

--- src/RefactorizacionProducto/GeneradorEtiqueta.java
+++ src/RefactorizacionProducto/GeneradorEtiqueta.java
@@ -1,5 +1,7 @@
 package RefactorizacionProducto;
 
 public class GeneradorEtiqueta {
-
+    public String generarEtiqueta(Producto producto, double precioFinal) {
+        return "Producto: " + producto.getNombre() + ", Precio: " + precioFinal;
+    }
 }
